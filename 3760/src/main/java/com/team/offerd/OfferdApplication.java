39baf6b5a3e8cea5c65f9ce6c7baa86e4a00e9ac
TheofilosP <theofilosssss@hotmail.com>
2017-11-07 19:25:50 +0200

    1

---
+++ 3760/src/main/java/com/team/offerd/OfferdApplication.java
@@ -0,0 +1,12 @@
+package com.team.offerd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfferdApplication.class, args);
+	}
+}
