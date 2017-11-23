787c470e32b3a9ca54ff4f69c4b638e8371a38bb
tangeli <vaggelhs_vlahos@hotmail.com>
2017-11-23 23:31:04 +0200

    aa
    Sdarova naproxoi

--- MyApplication/app/src/main/java/com/example/lazarakos/myapplication/Discount.java
+++ MyApplication/app/src/main/java/com/example/lazarakos/myapplication/Discount.java
@@ -2,6 +2,8 @@ package com.example.lazarakos.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Discount extends AppCompatActivity {
 
@@ -10,7 +12,17 @@ public class Discount extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discount);
 
-        startActivity(toy);
+
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.Discount_Prices, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        spinner.setAdapter(adapter);
 
     }
 }
--- MyApplication/app/src/main/java/com/example/lazarakos/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/lazarakos/myapplication/MainActivity.java
@@ -4,26 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button button4;
+    TextView text;
 
 
-
-    public Button button4;
-    public void init()
-    {
-        button4 = (Button)findViewById(R.id.button4);
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent toy = new Intent(MainActivity.this,Discount.class);
-
-            }
-        });
-
-    }
 
 
 
@@ -31,6 +22,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button4 = (Button)findViewById(R.id.button4);
+
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent neo = new Intent(MainActivity.this, Discount.class);
+                startActivity(neo);
+            }
+        });
+
 
 
 
